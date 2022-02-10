2e2f2785a01915fb29ff41e6a0ecc05071bf2c5b
Vinicius <vinioliveira.s@hotmail.com>
2022-02-10 18:47:24 -0300

    Fixing Main

--- multi_thread/Main.java
+++ multi_thread/Main.java
@@ -1,9 +1,24 @@
 package multi_thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class Main {
     public static void main(String[] args) {
-        ThreadClass t1 = new ThreadClass(1);
-        t1.start();
+        int i=0;
+        int ID=0;
+        while(i++<10){
+            long begin = System.currentTimeMillis();
+            
+            ExecutorService executor = Executors.newCachedThreadPool();
+            for (int j = 0; j < 4; j++) {
+                executor.execute(new ThreadClass(ID++));
+            }
+            executor.shutdown();
+            long end = System.currentTimeMillis();
+            System.out.println("Levou "+(end-begin)+" milisegundos");
+        }
     }
     
 }
