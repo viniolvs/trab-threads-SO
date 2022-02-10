015d03cd5e605c501a815409a69b3c01e9ac7e09
Vinicius <vinioliveira.s@hotmail.com>
2022-02-10 12:23:06 -0300

    Adding User class

---
+++ single-thread/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+    private int balance;
+
+    public String getName() {
+        return name;
+    }
+    public int getBalance() {
+        return balance;
+    }
+}
