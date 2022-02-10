9bc250dc14c28aef6495df32d839ffe91a3c682a
Vinicius <vinioliveira.s@hotmail.com>
2022-02-10 15:42:34 -0300

    Read and Write File classes

---
+++ single_thread/ReadFile.java
@@ -0,0 +1,42 @@
+package single_thread;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
+public class ReadFile {
+    private Scanner input;
+    
+
+    public void openFile() {
+        try {
+            input = new Scanner(new File("clients.txt"));
+        } catch (FileNotFoundException e) {
+            System.err.println("Error opening file.");
+            System.exit(1);
+        }
+    }
+
+
+    public Client readClientRecord() {
+        Client client = new Client();
+        try {
+            while(input.hasNext()){
+                client.setAccount(input.nextInt());
+                client.setName(input.next());
+                client.setBalance(input.nextInt());
+            }
+        } catch (NoSuchElementException e) {
+            System.err.println("File improperly formed");
+            input.close();
+            System.exit(1);
+        }
+        return client;
+    }
+
+    public void closeFile() {
+        if (input!=null)
+            input.close();
+    }
+}
---
+++ single_thread/WriteFile.java
@@ -0,0 +1,43 @@
+package single_thread;
+
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.FormatterClosedException;
+
+public class WriteFile {
+    private Formatter output;
+    
+    public void openFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("clients.txt",true);
+            output = new Formatter(fileWriter);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e){
+            System.out.println("File not found.");
+        }
+        
+    }
+
+    public void addRecord(int account, String name, int balance) {
+        try {
+            output.format("%d %s %d\n", account, name, balance);
+        } catch (FormatterClosedException e) {
+            System.out.println("Error writing on file");
+        }
+        
+    }
+
+    public void closeFile() {
+        if (output!=null)
+            output.close();
+    }
+
+    public void doItAll(int account, String name, int balance) {
+        openFile();
+        addRecord(account, name, balance);
+        closeFile();
+    }
+}
