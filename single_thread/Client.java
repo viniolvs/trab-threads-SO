1317a55b24bf98055457953278e89e0807a6b6de
Vinicius <vinioliveira.s@hotmail.com>
2022-02-13 12:47:46 -0300

    Changing completly the project to a library loan manager

--- single_thread/Client.java
+++ single_thread/Client.java
@@ -1,28 +1,14 @@
 package single_thread;
 
 public class Client{
-    private int accountID;
-    private int balance;
+    private String name;
+    private int clientID;
+    private Book[] loans;
 
-    public Client(int accountID){
-        this.accountID = accountID;
-        this.balance = GenerateBalance.getBalance();
+    public Client(int ID){
+        name = GenerateData.getName(ID);
+        clientID = ID;
+        
     }
-    public Client(int accountID, int balance){
-        this.accountID = accountID;
-        this.balance = balance;
-    }
-
-    public int getAccountID() {
-        return accountID;
-    }
-    protected void setAccountID(int accountID) {
-        this.accountID = accountID;
-    }
-    public int getBalance() {
-        return balance;
-    }
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
+    
 }
