0327c74a6be81680770db350a37e408cc6e24b15
Vinicius <vinioliveira.s@hotmail.com>
2022-02-13 13:31:01 -0300

    Start to implement LibraryManager class

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,5 +1,22 @@
 package src;
 
+import java.util.ArrayList;
+
 public class LibraryManager {
-    
+    private ArrayList<Client> clients;
+    private ArrayList<Book> books;
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void addClient (Client client) {
+        clients.add(client);
+    }
+    public void addBook(Book book) {
+        books.add(book);
+    }
 }
