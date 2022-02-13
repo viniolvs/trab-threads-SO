d1afa9ca048d26acd91c662c43d820d64d211040
Vinicius <vinioliveira.s@hotmail.com>
2022-02-13 12:48:56 -0300

    Iniciating Book class

---
+++ single_thread/Book.java
@@ -0,0 +1,33 @@
+package single_thread;
+
+public class Book {
+    private int bookID;
+    private String title;
+    private int year;
+    private String author;
+    private int edition;
+
+    public Book(int ID) {
+        bookID = ID;
+        title = GenerateData.getTitle(ID);
+        year = GenerateData.getYear();
+        edition = GenerateData.getEdition();
+        author = GenerateData.getAuthor(ID);
+    }
+
+    public int getID() {
+        return bookID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public int getEdition() {
+        return edition;
+    }
+}
