824d12b8de3f5995e44a19f7a812e457c17e8bb9
Vinicius <vinioliveira.s@hotmail.com>
2022-02-13 12:51:28 -0300

    Deleting unnecessary files and sections

--- multi_thread/Main.java
+++ multi_thread/Main.java
@@ -11,32 +11,8 @@ public class Main {
     public static void main(String[] args) {
         
         long begin = System.currentTimeMillis();
-
-               
-        /*ExecutorService executor = Executors.newCachedThreadPool();
-        for (int j = 0; j < 4; j++) {
-            Client client = new Client(j);
-            executor.execute(new WriterThread(client));    
-        }
-        executor.shutdown();*/
-        for (int i = 0; i < 100; i++) {
-            Client client = new Client(i);
-            WriterThread writer = new WriterThread(client);
-            ReaderThread reader = new ReaderThread(client);
-            
-            writer.start();
-            reader.start();
-            try {
-                writer.join();
-                reader.join();
-            } catch (InterruptedException e) {
-                System.out.println("Interrupted Exception");
-            }
-        }
         
 
-
-        
         long end = System.currentTimeMillis();
         System.out.println("Levou "+(end-begin)+" milisegundos");
     }
