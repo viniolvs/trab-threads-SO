7d58c2e7075cf792c1d3ac3f8ca5f19a8bbc56f1
Vinicius <vinioliveira.s@hotmail.com>
2022-02-14 23:06:03 -0300

    Changing data generator

--- src/GenerateData.java
+++ src/GenerateData.java
@@ -10,18 +10,48 @@ public class GenerateData {
     public static int getYear() {
         return random.nextInt(2022)+1900;
     }
-    public static int getEdition() {
-        return random.nextInt(20);
+    public static String getPlate() {
+        String plate = "0000000";
+        for (int i = 0; i < plate.length(); i++) {
+            plate.replaceFirst("0", Integer.toString(random.nextInt(9))); 
+        }
+        return plate;
     }
-    public static String getTitle(int ID) {
-        return "book "+ Integer.toString(ID);
+    public static String getModel(int ID) {
+        return "model "+ Integer.toString(ID);
     }
-    public static String getAuthor(int ID) {
-        return "author "+ Integer.toString(ID);
+    public static String getColor(int ID) {
+        if (ID%2 == 0) 
+            return "White";
+        else
+            return "Black";
     }
 
-    //clientdata
+    //User data
     public static String getName(int ID) {
         return "client "+ Integer.toString(ID);
+    }
+    public static String getCPF() {
+        String CPF = "000.000.000-00";
+        for (int i = 0; i < CPF.length(); i++) {
+            CPF.replaceFirst("0", Integer.toString(random.nextInt(9))); 
+        }
+        return CPF;
+    }
+
+    public static String getDriverLicense() {
+        String cnh = "00000000000";
+        for (int i = 0; i < cnh.length(); i++) {
+            cnh.replaceFirst("0", Integer.toString(random.nextInt(9))); 
+        }
+        return cnh;
+    }
+
+    public static String getBankAccount() {
+        String account = "00000-0";
+        for (int i = 0; i < account.length(); i++) {
+            account.replaceFirst("0", Integer.toString(random.nextInt(9))); 
+        }
+        return account;
     }
 }
